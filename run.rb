8039e1e1b0d31f473ad4f5649f4a1338ab0d697c
Vaibhav Aher <vaher@infoblox.com>
2016-08-27 13:55:59 -0700

    Resolved run issues

--- run.rb
+++ run.rb
@@ -1,11 +1,11 @@
-	#!/usr/bin/ruby
-	require './nasa_controller'
+#!/usr/bin/ruby
+require './nasa_controller'
 
-	begin
-		main = NasaController.new([ "5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"])
-		main.validate_rovers_commands
-		main.start_rovers_commands
-		MarsPlateau.rovers.each_with_index {|rover, index| print "Rover #{index+1}: #{rover.to_string()}"}
-	rescue RuntimeError => e
-		print "#{e.class} => #{e.inspect}"
-	end
+begin
+	main = NasaController.new([ "5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"])
+	main.validate_rovers_commands
+	main.start_rovers_commands
+	MarsPlateau.rovers.each_with_index {|rover, index| print "Rover #{index+1}: #{rover.to_string()}"}
+rescue RuntimeError => e
+	print "#{e.class} => #{e.inspect}"
+end
